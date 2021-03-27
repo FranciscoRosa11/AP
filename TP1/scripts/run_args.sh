23e7341630a1d173de2c7e7e3d057f05ba36d341
FranciscoRosa11 <a80749@alunos.uminho.pt>
2021-03-27 21:57:13 +0000

    added steps to script

--- TP1/scripts/run_args.sh
+++ TP1/scripts/run_args.sh
@@ -16,9 +16,12 @@ do
     do
         for a in {0.999, 0.899, 0.799, 0.699, 0.599}
         do
-            echo "N = $i and P = $j"
-            ./rooms_sa --persons $i --processes 1 --temp $j --annealing $a --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
-            ./rooms_mc --persons $i --processes 1 --temp $j --annealing $a --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+            for s in {25, 50, 75, 100, 125, 150}
+            do
+                echo "N = $i, T = $j, A = $a and S = $s"
+                ./rooms_sa --persons $i --processes 1 --temp $j --annealing $a --steps $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+                ./rooms_mc --persons $i --processes 1 --temp $j --annealing $a --steps $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+            done
         done
     done
 done
