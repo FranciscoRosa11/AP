870fe4f6b006aa287ac77d972304f86400141a53
FranciscoRosa11 <a80749@alunos.uminho.pt>
2021-03-28 10:50:17 +0100

    run args updated

--- TP1/scripts/run_args.sh
+++ TP1/scripts/run_args.sh
@@ -1,26 +1,29 @@
-source set_env.sh
+current_dir=$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd)
+source ${current_dir}/set_env.sh
+
+source setup.sh
 mkdir $PROJECT_METRICS_DIR
 
 cd ../..
-
-make clean
-make
 
 cd TP1/bin/
 
 echo "Results:"
 
-for i in {100,200,300,400,500,1000}
+for i in {100,200,300,400}
 do
-    for j in {1.1,1.2,1.3,1.4,1.5}
+    for j in {1.1,1.2,1.3}
     do
-        for a in {0.999, 0.899, 0.799, 0.699, 0.599}
+        for a in {0.999,0.899,0.799}
         do
-            for s in {25, 50, 75, 100, 125, 150}
+            for s in {90,100,101}
             do
-                echo "N = $i, T = $j, A = $a and S = $s"
-                ./rooms_sa --persons $i --processes 1 --temp $j --annealing $a --steps $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
-                ./rooms_mc --persons $i --processes 1 --temp $j --annealing $a --steps $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+                
+                    echo "N = $i, T = $j, A = $a and S = $s"
+                    ${PROJECT_BIN_DIR}/rooms_sa --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+                    ${PROJECT_BIN_DIR}/rooms_mc --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+                    ${PROJECT_BIN_DIR}/rooms_greedy --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+                    ${PROJECT_BIN_DIR}/rooms_greedy_person --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
             done
         done
     done
