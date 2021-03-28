ef65d93721a8bf109b92e9dc71db255505f84782
FranciscoRosa11 <a80749@alunos.uminho.pt>
2021-03-28 11:16:38 +0100

    run args P=10

--- TP1/scripts/run_args.sh
+++ TP1/scripts/run_args.sh
@@ -10,20 +10,20 @@ cd TP1/bin/
 
 echo "Results:"
 
-for i in {100,200,300,400}
+for i in {100,200,300,400,500,1000,2000}
 do
-    for j in {1.1,1.2,1.3}
+    for j in {1.1,1.2,1.3,1.4,1.5,1.6}
     do
-        for a in {0.999,0.899,0.799}
+        for a in {0.999,0.899,0.799,0.699,0.599}
         do
-            for s in {90,100,101}
+            for s in {90,100,110}
             do
                 
                     echo "N = $i, T = $j, A = $a and S = $s"
-                    ${PROJECT_BIN_DIR}/rooms_sa --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
-                    ${PROJECT_BIN_DIR}/rooms_mc --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
-                    ${PROJECT_BIN_DIR}/rooms_greedy --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
-                    ${PROJECT_BIN_DIR}/rooms_greedy_person --persons $i --processes 4 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test --debug
+                    ${PROJECT_BIN_DIR}/rooms_sa --persons $i --processes 10 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test
+                    ${PROJECT_BIN_DIR}/rooms_mc --persons $i --processes 10 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test
+                    ${PROJECT_BIN_DIR}/rooms_greedy --persons $i --processes 10 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test
+                    ${PROJECT_BIN_DIR}/rooms_greedy_person --persons $i --processes 10 --temp $j --annealing $a --stability $s --metrics $PROJECT_METRICS_DIR/report.csv --error --label test
             done
         done
     done
